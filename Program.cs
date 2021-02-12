32c526e1dd5806fbdb1ae7f724b5618e818e984a
Nohemí <nohemi1180212345@gmail.com>
2021-02-11 22:11:28 -0700

    Se creó un Hello World invertido

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Inverse_Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             string[] world = new string [6] {"!", "d", "l", "r", "o", "W"};
+
+       for (int i = 0; i < world.Length; i++)
+       {
+           Console.WriteLine (world[i]);
+       }    
+       
+       string[] hello = new string [5] {"o", "l", "l", "e", "H"};
+
+       for (int i = 0; i < hello.Length; i++)
+       {
+           Console.WriteLine (hello[i]);
+       }
+        }
+    }
+}
